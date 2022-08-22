8a4ffec92b5686d3ef03226d75973cc0a6993249
estif95 <25502990+estif95@users.noreply.github.com>
2022-08-22 13:21:16 +0200

    terragrunt-config

--- dev/services/codely-sqs/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "codely-tf-states"
-    key            = "dev/services/codely-sqs"
-    region         = "eu-west-1"
-    encrypt        = true
-    dynamodb_table = "codely-tf-states"
-  }
-}
--- dev/services/codely-sqs/main.tf
+++ dev/services/codely-sqs/main.tf
@@ -1,5 +1,5 @@
 #resource "aws_sqs_queue" "terraform_queue" {
-#  name                      = "dev-codely-sqs"
+#  name                      = "${var.stage}-codely-sqs"
 #  delay_seconds             = 90
 #  max_message_size          = 2048
 #  message_retention_seconds = 86400
