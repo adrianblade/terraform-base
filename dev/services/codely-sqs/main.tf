34343f6d18eb7f1e87c7d4e7b75775d1f6eaba5f
estif95 <25502990+estif95@users.noreply.github.com>
2022-08-22 14:43:26 +0200

    terragrunt

--- dev/services/codely-sqs/main.tf
+++ dev/services/codely-sqs/main.tf
@@ -1,14 +1,14 @@
-#resource "aws_sqs_queue" "terraform_queue" {
-#  name                      = "${var.stage}-codely-sqs"
-#  delay_seconds             = 90
-#  max_message_size          = 2048
-#  message_retention_seconds = 86400
-#  receive_wait_time_seconds = 10
-#
-#  tags = {
-#    stage       = "dev"
-#    terraform   = "true"
-#    accountable = "development"
-#   domain      = "codely"
-#  }
-#}
+resource "aws_sqs_queue" "terraform_queue" {
+  name                      = "${var.stage}-codely-sqs"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+
+  tags = {
+    stage       = "dev"
+    terraform   = "true"
+    accountable = "development"
+   domain      = "codely"
+  }
+}
