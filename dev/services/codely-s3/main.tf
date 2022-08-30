6c39f1a108170fd2f845790803f30ad25fb1ae31
estif95 <25502990+estif95@users.noreply.github.com>
2022-08-30 15:20:09 +0200

    testing tfsec

--- dev/services/codely-s3/main.tf
+++ dev/services/codely-s3/main.tf
@@ -3,6 +3,14 @@ resource "aws_s3_bucket" "main" {
   bucket = "my-codely-test-bucket"
   acl    = "public-read"
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = {
     stage       = var.stage
     terraform   = "true"
