e7107403aa338b2d3c70fedb58c6dd1c132245ab
estif95 <25502990+estif95@users.noreply.github.com>
2022-08-30 15:59:29 +0200

    test infracost comments

--- dev/services/codely-s3/main.tf
+++ dev/services/codely-s3/main.tf
@@ -13,7 +13,31 @@ resource "aws_s3_bucket" "main" {
 
   tags = {
     stage       = var.stage
-    terraform   = "true"
+    terraform   = "false"
+    accountable = "development"
+    domain      = "codely"
+  }
+
+  provider = aws.mango-test-ireland
+
+}
+
+resource "aws_s3_bucket" "main" {
+
+  bucket = "my-codely-test-bucket2"
+  acl    = "public-read"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    stage       = var.stage
+    terraform   = "false"
     accountable = "development"
     domain      = "codely"
   }
