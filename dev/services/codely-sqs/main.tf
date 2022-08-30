230b8208792a3739daa402327e07038ad55d3c73
estif95 <25502990+estif95@users.noreply.github.com>
2022-08-30 12:12:28 +0200

    test tfsec

--- dev/services/codely-sqs/main.tf
+++ dev/services/codely-sqs/main.tf
@@ -1,9 +1,11 @@
 resource "aws_sqs_queue" "terraform_queue" {
   name                      = "${var.stage}-codely-sqs"
   delay_seconds             = 90
-  max_message_size                    = 2048
+  max_message_size          = 2048
   message_retention_seconds = 86400
-        receive_wait_time_seconds = 10
+  receive_wait_time_seconds = 10
+
+  policy = aws_sqs_queue_policy.queue_policy.policy
 
   tags = {
     stage       = var.stage
@@ -13,4 +15,24 @@ resource "aws_sqs_queue" "terraform_queue" {
   }
 
   provider = aws.mango-test-ireland
+}
+
+resource "aws_sqs_queue_policy" "queue_policy" {
+  queue_url = aws_sqs_queue.q.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "*",
+      "Resource": "*",
+    }
+  ]
+}
+POLICY
 }
