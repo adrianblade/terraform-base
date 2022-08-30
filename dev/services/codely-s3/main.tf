cafc530ea62c7f3142be6e711e006a35eca6bbcd
estif95 <25502990+estif95@users.noreply.github.com>
2022-08-30 15:10:33 +0200

    change ec2 to s3 and public ACL

---
+++ dev/services/codely-s3/main.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "main" {
+
+  bucket = "my-codely-test-bucket"
+  acl    = "public-read"
+
+  tags = {
+    stage       = var.stage
+    terraform   = "true"
+    accountable = "development"
+    domain      = "codely"
+  }
+
+  provider = aws.mango-test-ireland
+  
+}
