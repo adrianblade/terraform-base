0d8c81ba675a133060c15cef7cf36e65f0ec70df
estif95 <25502990+estif95@users.noreply.github.com>
2022-08-30 11:31:56 +0200

    rename workflow and test fmt

--- dev/services/codely-sqs/main.tf
+++ dev/services/codely-sqs/main.tf
@@ -1,9 +1,9 @@
 resource "aws_sqs_queue" "terraform_queue" {
   name                      = "${var.stage}-codely-sqs"
   delay_seconds             = 90
-  max_message_size          = 2048
+  max_message_size                    = 2048
   message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
+        receive_wait_time_seconds = 10
 
   tags = {
     stage       = var.stage
