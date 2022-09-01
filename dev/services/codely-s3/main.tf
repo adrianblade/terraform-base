a0e533957dcf1fec09c912226cf4f58f7fa5e03e
estif95 <25502990+estif95@users.noreply.github.com>
2022-09-01 11:40:16 +0200

    Test PR

--- dev/services/codely-s3/main.tf
+++ dev/services/codely-s3/main.tf
@@ -1,26 +1,27 @@
-#resource "aws_s3_bucket" "main" {
-#
-#  bucket = "my-codely-test-bucket"
-#  acl    = "authenticated-read"
-#
-#  server_side_encryption_configuration {
-#    rule {
-#      apply_server_side_encryption_by_default {
-#        sse_algorithm = "AES256"
-#      }
-#    }
-#  }
-#
-#  tags = {
-#    stage       = var.stage
-#    terraform   = "false"
-#    accountable = "development"
-#    domain      = "codely"
-#  }
-#
-#  provider = aws.mango-test-ireland
-#
-#}
+resource "aws_s3_bucket" "main" {
+
+  bucket = "my-codely-test-bucket"
+  acl    = "authenticated-read"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    stage       = var.stage
+    terraform   = "false"
+    accountable = "development"
+    domain      = "codely"
+  }
+
+  provider = aws.mango-test-ireland
+  
+
+}
 
 
 data "aws_ami" "ubuntu" {
