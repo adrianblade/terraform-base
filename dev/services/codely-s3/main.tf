9e9c0f5181457dce7d8ad7048077b404f1a1ac72
Adrian Catalan <adrian.catalan@mango.com>
2022-09-02 11:20:31 +0200

    Change delay seconds

--- dev/services/codely-s3/main.tf
+++ dev/services/codely-s3/main.tf
@@ -1,7 +1,7 @@
 resource "aws_sqs_queue" "terraform_queue" {
   name                      = "terraform-example-queue"
-  delay_seconds             = 90
-  max_message_size          = 2048
+        delay_seconds             = 91
+      max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
 
