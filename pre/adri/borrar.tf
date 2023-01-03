9b1f877e289a042c2c9897293ed395f2d9324ce2
estif95 <25502990+estif95@users.noreply.github.com>
2023-01-03 16:16:34 +0100

    test full lifecycle

--- pre/adri/borrar.tf
+++ pre/adri/borrar.tf
@@ -1,1 +1,8 @@
-resource "null_resource" "cluster" {}
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-borrar-delete"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
