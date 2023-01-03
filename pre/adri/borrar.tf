6c4ce839ac5148e7e83c12928500d65a1f47e83a
estif95 <25502990+estif95@users.noreply.github.com>
2023-01-03 17:09:27 +0100

    create secret

--- pre/adri/borrar.tf
+++ pre/adri/borrar.tf
@@ -1,8 +1,3 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-borrar-delete"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+resource "aws_secretsmanager_secret" "example" {
+  name = "example"
 }
